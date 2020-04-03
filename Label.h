de3d0823fb3cefc710f6b0f3bcc35b2fc22112f8
xypwn <darwin@Arch.localdomain>
2020-04-03 19:37:54 +0200

    Add saveable labels for parts and connectors

---
+++ Label.h
@@ -0,0 +1,11 @@
+#ifndef LABEL_H
+#define LABEL_H
+
+
+class Label : public QGraphicsTextItem
+{
+public:
+    Label();
+};
+
+#endif // LABEL_H
---
+++ eAlignMode.h
@@ -0,0 +1,4 @@
+#ifndef EALIGNMODE_H
+#define EALIGNMODE_H
+
+#endif // EALIGNMODE_H
